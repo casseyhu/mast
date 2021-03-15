334f9dff490eae992d71b834a65bf982b920736a
huiga <eddiex2205@gmail.com>
2021-03-15 19:36:50 -0400

    changed landing page

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class MainPage extends Component {
+    render(){
+        return (
+            <>
+            <div>
+                <p>This is the mast. this is it.</p>
+            </div>
+            </>
+        )
+    }
+}
+
+export default MainPage;
