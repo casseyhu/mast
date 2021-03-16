3fb13cbca751cc058d664612c56c5c5494dec9e0
casseyhu <casseyhu123@gmail.com>
2021-03-16 13:44:10 -0400

    import file

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
 const Button = (props) => {
+    const hiddenFileInput = React.useRef(null);
+
+    const handleClick = event => {
+        if (props.variant === "square")
+            hiddenFileInput.current.click();
+        // props.onClick();
+    };
+
+    const handleChange = event => {
+        const file = event.target.files[0];
+        props.setFile(file);
+        // props.handleFile(file);
+        console.log(file);
+    };
+
     return (
         <button className={`${props.variant}-button`} 
-            onClick={props.onClick}>
+            onClick={handleClick}>
             {props.text}
+            {(props.variant === "square") && 
+            <input
+            type="file"
+            ref={hiddenFileInput}
+            onChange={handleChange}
+            style={{display: 'none'}} 
+            />
+            }
         </button>
     )
 }
--- src/components/ImportItem.jsx
+++ src/components/ImportItem.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Button from './Button';
 import Dropdown from './Dropdown';
 
 const ImportItem = (props) => {
 
+    const [file, setfile] =  useState("");
+    const [firstfile, setfirstfile] =  useState("");
 
-
-    
     return (
         <div style={{margin:"1.5rem 0"}}>
             <h4>{props.header}</h4>
@@ -14,8 +14,9 @@ const ImportItem = (props) => {
             <div className="flex-horizontal">
                 <span style={{width:"150px"}}>{props.first}</span>
                 <div style={{width:"20"}}>
-                    <Button variant="square" text="Browse My Computer"/>
+                    <Button variant="square" text="Browse My Computer" setFile={e => setfirstfile(e)}/>
                 </div>
+                <small style={{marginLeft:"1.5rem"}}>{firstfile.name}</small>
             </div>}
             {props.dropdown &&
             <div className="flex-horizontal">
@@ -27,12 +28,13 @@ const ImportItem = (props) => {
             <div className="flex-horizontal">
                 <span style={{width:"150px"}}>{props.type}</span>
                 <div style={{width:"20"}}>
-                    <Button variant="square" text="Browse My Computer"/>
+                    <Button variant="square" text="Browse My Computer" setFile={e => setfile(e)}/>
                 </div>
-                <div style={{width:"15%", marginLeft:"1.4rem"}}>
+                <div style={{width:"100px", marginLeft:"1.4rem"}}>
                     <Button variant="round" text="Upload" />
                 </div>
             </div>
+            <small style={{marginLeft:"150px"}}>{file.name}</small>
         </div>
     )
 }
