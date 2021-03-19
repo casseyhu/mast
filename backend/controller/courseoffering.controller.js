f79a5ad1f7a8e38ca7d98306de2025c28778be53
huiga <eddiex2205@gmail.com>
2021-03-19 18:08:40 -0400

    merging w cas changes

--- backend/controller/courseoffering.controller.js
+++ backend/controller/courseoffering.controller.js
@@ -19,9 +19,25 @@ exports.upload = (req, res) => {
                     // console.log(typeof(results.data[0].year))
                     // console.log(typeof(results.data[0].semester))
                     console.log("Finished reading CourseOffering file")
-                    deleteSemestersFromDB(results)
-                    uploadNewOfferings(results)
-                    console.log("Done importing all courses from csv")
+                    var del_semesters = new Promise((resolve, reject) => {
+                        if(deleteSemestersFromDB(results)){
+                            resolve(results)
+                        }
+
+                    })
+                    del_semesters.then(res => {
+                        var upload_courses = new Promise((resolve, reject) => {
+                            if(uploadNewOfferings(results)){
+                                resolve(results)
+                            }
+                        })
+                        upload_courses.then(res => {
+                            console.log("Done importing all courses from csv")
+                        })
+                    })
+                    // The "done importing" runs before the insert/deletes even happen
+                    // becasue of the async. Maybe change it to sync? 
+                    // console.log("Done importing all courses from csv")
 
                 }
             })
@@ -50,7 +66,8 @@ exports.upload = (req, res) => {
 // Everyone will have to DORP TABLE courseofferings, then rerun the 
 // server to recreate the table through sequelize. 
 
-async function uploadNewOfferings(csv_file){
+function uploadNewOfferings(csv_file){
+    console.log('runnig upload')
     for(let i = 0; i < csv_file.data.length; i++){
         course = csv_file.data[i]
         csv_timeslot = (course.timeslot ? course.timeslot.split(' ') : null)
@@ -58,9 +75,7 @@ async function uploadNewOfferings(csv_file){
         time_range = (csv_timeslot ? csv_timeslot[1].split('-') : null)
         start = (time_range ? moment(time_range[0], ['h:mmA']).format('HH:mm') : null)
         end = (time_range ? moment(time_range[1], ['h:mmA']).format('HH:mm') : null)
-
-        // console.log(day, start, end)
-        await CourseOffering.create({
+        CourseOffering.create({
             identifier: course.department+course.course_num,
             semester: course.semester,
             year: course.year,
@@ -70,7 +85,7 @@ async function uploadNewOfferings(csv_file){
             endTime: end
         })
     }
-    return
+    return true
 }
 
 
@@ -80,7 +95,7 @@ async function uploadNewOfferings(csv_file){
 // where the semester+year(s) are covered by this new CSV.
 // Will return a promise after the await is done. Try to
 // catch it in the main loop and handle it in there. 
-async function deleteSemestersFromDB(csv_file){
+function deleteSemestersFromDB(csv_file){
     scraped_semesters = []
     for(let i = 0; i < csv_file.data.length; i++){
         course = csv_file.data[i]
@@ -88,12 +103,11 @@ async function deleteSemestersFromDB(csv_file){
             scraped_semesters.push(course.semester+' '+course.year)
         }
     }
-    console.log(scraped_semesters)
     for(let i = 0; i < scraped_semesters.length; i++){
         semyear = scraped_semesters[i].split(' ')
         // Might have to CASCADE the deletes to the 
         // CoursePlans that reference these courses (?)
-        await CourseOffering.destroy({
+        CourseOffering.destroy({
             where: { 
                 semester: semyear[0],
                 year: Number(semyear[1])
@@ -101,9 +115,10 @@ async function deleteSemestersFromDB(csv_file){
         })
     }
     console.log("Done deleting all scraped semesters from db")
-    return
+    return true
 }
 
 // https://stackoverflow.com/questions/16336367/what-is-the-difference-between-synchronous-and-asynchronous-programming-in-node
 // https://hackernoon.com/understanding-async-await-in-javascript-1d81bb079b2c
 // https://hackernoon.com/understanding-promises-in-javascript-13d99df067c1
+// https://stackoverflow.com/questions/26783080/convert-12-hour-am-pm-string-to-24-date-object-using-moment-js
